251a2221075664588bad0e5e4d1420fecc22bcb8
ranguangyu <ranguangyu@zbj.com>
2016-10-28 11:22:33 +0800

    简易js代码浏览器完成

--- KylinWidgets/ky1.0/kcodeview（代码浏览器）/kcodeview.js
+++ KylinWidgets/ky1.0/kcodeview（代码浏览器）/kcodeview.js
@@ -2,14 +2,14 @@ function Kcodeview(bar){
 
 	var param = {};
 
+	var operators = ['\\+=', '-=', '\\*=', '\\/=', '&lt;=', '&gt;=', '&lt;', '&gt;', '=', '\\+', '-', '\\*', '\\/', '%'];
+
 	var colors = {
-		color1: ['var', 'let', 'for', 'if', 'else', 'return', 'function', 'this', 'in', 'switch'],
-		color2: ['log', 'push', 'shift', 'pop', 'unshift'],
+		color1: ['var', 'let', 'for', 'if', 'else', 'return', 'function', 'this', 'in', 'switch', 'new'],
+		color2: ['log', 'push', 'shift', 'pop', 'unshift', 'join', 'concat', 'match', 'replace'],
 		color3: ['console', 'RegExp', 'Date', 'Array', 'Object'],
 		color4: ['prototype']
 	};
-
-	// '-', '*', '\/', '%', '+=', '-=', '*=', '\/='
 
 	var initEle = function() {
 		param.code = ky.select(bar, 'code');
@@ -21,18 +21,29 @@ function Kcodeview(bar){
 	var init = function(obj) {
 		initEle();
 
+		// 参数配置
+		ky.CssUtil.setCss(param.code, {
+			'font-family': 'consolas'
+		});
 		ky.CssUtil.setCss(param.code, obj);
 
+		// 开始着色
 		var res = param.code.innerHTML;
-
+		// 符号 + 字符串 着色
+		res = res.replace(new RegExp(operators.join('|').concat("|\".*\"|'.*'"), 'g'), function(tar) {
+			if(tar.match(/"|'/)) {
+				return '<span class="color4">' + tar + '</span>';
+			}else{
+				return '<span class="color2">' + tar + '</span>';
+			}
+		});
+		// 关键字着色
 		for (var key in colors) {
 			res = res.replace(new RegExp(colors[key].join('|'), 'g'), function(tar) {
 				return '<span class="' + key + '">' + tar + '</span>';
 			});
 		}
-
 		param.code.innerHTML = res;
-
 	};
 
 	return {
