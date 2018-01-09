8aa58d40b320e9b646b5bc38fdd62f92fb0b0318
冉光宇 <ranguangyu@zbj.com>
2018-01-09 11:23:47 +0800

    new kcountdowntimer

---
+++ KjQueryWidgets/kcountdowntimer（倒计时组件）/kcountdowntimer.js
@@ -0,0 +1,41 @@
+/*
+ * @desc 倒计时组件
+ * @author rgy19930329
+ * @date 2018-01-09
+ **/
+
+$ && (function($) {
+  $.fn.extend({
+    /*count down timer*/
+    cdtimer: function(opts) {
+      var $trigger = $(this);
+
+      var total = opts.total || 60;
+      var display = opts.display || '重发验证码';
+      var displayTemplate = opts.displayTemplate || '{second}s';
+      var defaultTemplate = opts.defaultTemplate || '发送验证码';
+      var disabledClassName = opts.disabledClassName || 'verif-code-disabled';
+      var before = opts.before || function() {};
+
+      $trigger.html(defaultTemplate);
+
+      $trigger.on('click', function() {
+        before && before(function() {
+          var second = parseInt(total);
+          $trigger.html(displayTemplate.replace(/\{second\}/g, second--)).addClass(disabledClassName).prop('disabled', true);
+          var timer = setInterval(function() {
+            $trigger.html(displayTemplate.replace(/\{second\}/g, second--));
+            if (second == 0) {
+              $trigger.html(display).removeClass(disabledClassName).prop('disabled', false);
+
+              clearInterval(timer);
+              timer = null;
+            }
+          }, 1000);
+        }, function() {
+          // console.log('fail');
+        });
+      });
+    }
+  });
+})(jQuery);
